e0c4fdd8503f04ff205c881ff24fab3702f41ea9
hubowen1 <54797640+hubowen1@users.noreply.github.com>
2019-09-10 15:15:06 +0800

    Add files via upload

---
+++ ex1.java
@@ -0,0 +1,7 @@
+package ex1;
+
+public class ex1 {
+  public static void main(String[] args) {
+	  System.out.println((10.5 + 2 * 3) / (45 - 3.5));
+  }
+}
---
+++ ex2.java
@@ -0,0 +1,19 @@
+package ex1;
+
+public class ex2 {
+  public static void main(String[] args) {
+	  double radius; //Declare radius
+	  double area; //Declare area
+	  
+	  //Assign a radius 
+	  radius = 20; //radius is now 20
+	  
+	  
+	  //Compute area
+	  area = radius * radius * 3.14159;
+	  
+	  
+	  //Display result
+	  System.out.println("The area for the circle of radius " + radius +" is " + area);
+  }
+}
