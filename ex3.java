bd9278121f71b7f31df33061195ef574eae828dd
hubowen1 <54797640+hubowen1@users.noreply.github.com>
2019-09-17 15:36:20 +0800

    Add files via upload

---
+++ ex3.java
@@ -0,0 +1,27 @@
+package ex2;
+
+
+import java.util.Scanner;
+
+
+public class ex3 {
+
+	public static void main(String[] args) {
+		int number1 = (int)(System.currentTimeMillis() % 10);
+		int number2 = (int)(System.currentTimeMillis() / 7 % 10);
+		
+		//Create a Scanner
+		Scanner input = new Scanner(System.in);
+		
+		
+		System.out.print(
+				"What is " + number1 + " + " + number2 + "? " );
+		
+		int answer = input.nextInt();
+		
+		System.out.println(
+				number1 + " + "  + number2 + " = " + answer + " is " + (number1 + number2 == answer));
+
+	}
+
+}
